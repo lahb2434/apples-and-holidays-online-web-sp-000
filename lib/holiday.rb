c1807e8292e0fb33a759ea1a2350dc61236b114f
Chris Houghton <chrishoughton2434@gmail.com>
2020-09-18 14:31:07 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,7 +53,7 @@ def all_holidays_with_bbq(holiday_hash)
     holiday_hash.each do |season, holidays|
       holidays.each do |holiday, items|
         if items.include? "BBQ"
-          puts "#{holiday}"
+          barbeque << holid
           
         end
       end
